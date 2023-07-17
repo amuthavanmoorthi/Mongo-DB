8c7a107e95d4ed72e404b05cf73ed38c581b3508
Amudhavan M <53816364+amuthavanmoorthi@users.noreply.github.com>
2023-07-17 22:30:20 +0530

    java basic

---
+++ task3.java
@@ -0,0 +1,71 @@
+class task3{
+
+int a=5;
+int b=5;
+
+short c=5;
+short d=6;
+
+byte e=2;
+byte f=4;
+
+long g=3;
+long h=9;
+
+Float i = 3.2f;
+Float j = 8.4f;
+
+double m=3.2;
+double n=6.7;
+
+public static void main(String args[]){
+
+task3 obj = new task3();
+
+System.out.println("This Object Model");
+
+System.out.println("Integeral Datatyoe");
+System.out.println(obj.a+obj.b);
+System.out.println(obj.a-obj.b);
+System.out.println(obj.a*obj.b);
+System.out.println(obj.a/obj.b);
+System.out.println(obj.a%obj.b);
+
+System.out.println("Short Datatype");
+System.out.println(obj.c+obj.d);
+System.out.println(obj.c*obj.d);
+System.out.println(obj.c/obj.d);
+System.out.println(obj.c-obj.d);
+System.out.println(obj.c%obj.d);
+
+System.out.println("byte Datatype");
+System.out.println(obj.e+obj.f);
+System.out.println(obj.e*obj.f);
+System.out.println(obj.e/obj.f);
+System.out.println(obj.e-obj.f);
+System.out.println(obj.e%obj.f);
+
+System.out.println("Long Datatype");
+System.out.println(obj.g+obj.h);
+System.out.println(obj.g*obj.h);
+System.out.println(obj.g/obj.h);
+System.out.println(obj.g-obj.h);
+System.out.println(obj.g%obj.h);
+
+System.out.println("Float Datatype");
+System.out.println(obj.i+obj.j);
+System.out.println(obj.i*obj.j);
+System.out.println(obj.i/obj.j);
+System.out.println(obj.i-obj.j);
+System.out.println(obj.i%obj.j);
+
+System.out.println("Double Datatype");
+System.out.println(obj.m+obj.n);
+System.out.println(obj.m*obj.n);
+System.out.println(obj.m/obj.n);
+System.out.println(obj.m-obj.n);
+System.out.println(obj.m%obj.n);
+
+
+}
+}
